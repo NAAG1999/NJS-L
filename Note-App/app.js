a3ba21c3fa1f52d8dcaf6c249e18bf916ef58360
Nilesh <agnilesh8199@gmail.com>
2018-06-11 10:41:50 +0530

    Menu driven

--- Note-App/app.js
+++ Note-App/app.js
@@ -5,3 +5,18 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');//modules we created
 
+var command = process.argv[2];// returns an array containing the command line arguments passed to it
+console.log('Command:', command);
+
+if(command === 'add'){
+    console.log('Adding new note');
+}   else if(command === 'list'){
+    console.log('Listing all nodes');
+}   else if(command === 'read'){
+    console.log('Reading note');
+}   else if(command === 'remove'){
+    console.log('Removing note');
+}   else{
+    console.log('Command not found!');
+}
+    
