d78a6d824aa7083be571802815793f60f5fc0746
Nilesh <agnilesh8199@gmail.com>
2018-06-13 23:04:29 +0530

    Asynchronous-basics

---
+++ Weather-App/async-basics.js
@@ -0,0 +1,10 @@
+console.log('Starting app');
+
+setTimeout(() =>{
+    console.log('Hello there !');
+}, 2000); // time to be given in mili seconds
+
+setTimeout(() =>{
+    console.log('Second Time out delay works!!')
+}, 0);
+console.log('Finsihing app');
