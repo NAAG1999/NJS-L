238f24afa635081c571de5aeffc0e0a393099b2d
Nilesh <agnilesh8199@gmail.com>
2018-06-14 22:21:39 +0530

    Adding comments

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -6,3 +6,6 @@ request({
 }, (error, response, body) =>{
     console.log(JSON.stringify(body, undefined, 2));
 });
+//in the console log statement we can swap body with error, response and body itself, to print the required element in our output
+//body is a part of http request: core data that comes from the http request
+//error contains error related to our http request ex: domain name is wrong
