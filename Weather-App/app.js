fe736a03411dae1ba64e669e9afe90823bcf3cf7
Nilesh <agnilesh8199@gmail.com>
2018-06-14 22:34:10 +0530

    Printing specified details only

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -4,7 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA',
     json: true 
 }, (error, response, body) =>{
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log('Address: ${body.results[0].formatted_address}');
 });
 //in the console log statement we can swap body with error, response and body itself, to print the required element in our output
 //body is a part of http request: core data that comes from the http request
