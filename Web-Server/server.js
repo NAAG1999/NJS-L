702423987f809518cc9795947bc6db2d087758b6
Nilesh <agnilesh8199@gmail.com>
2018-06-15 23:22:38 +0530

    Adding more pages to server

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'hbs');
+
 app.get('/', function(req, res){
-    //res.send('<h1>GET request to the homepage</h1>');
-    res.send({
-        name: 'Nilesh',
-        age: 18,
-        likes: [
-            'coding',
-            'playing'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Welcome Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Hello User'
     });
-});
+})
+
+app.get('/about', function(req,res){
+    res.render('about.hbs',{    //injecting data using render method
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+})
 
 app.get('/bad', function(req,res){
     res.send({
