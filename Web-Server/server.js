fa94a50a0e16124b02a75352922a80858d70a4ec
Nilesh <agnilesh8199@gmail.com>
2018-06-15 22:10:49 +0530

    Sending back the JSON data

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -3,7 +3,15 @@ const express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('<h1>GET request to the homepage</h1>');
+    //res.send('<h1>GET request to the homepage</h1>');
+    res.send({
+        name: 'Nilesh',
+        age: 18,
+        likes: [
+            'coding',
+            'playing'
+        ]
+    });
 })
 
 app.listen(3000);
