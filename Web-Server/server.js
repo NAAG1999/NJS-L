2cea819bb46fb23818bc63e18ec4d6def6c1c213
Nilesh <agnilesh8199@gmail.com>
2018-06-15 22:06:22 +0530

    Creating styles

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('GET request to the homepage');
+    res.send('<h1>GET request to the homepage</h1>');
 })
 
 app.listen(3000);
