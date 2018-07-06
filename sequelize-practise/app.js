19f19e4f436e4a718f719f0643efbd7815ef838c
Nilesh <agnilesh8199@gmail.com>
2018-07-07 02:25:03 +0530

    Setting up the datatypes and model of the db

--- sequelize-practise/app.js
+++ sequelize-practise/app.js
@@ -1,2 +1,7 @@
 var Sequelize = require('sequelize')
 var connection = new Sequelize('demo_schema', 'root', 'password'); // demo_schmea --> name of database, username, password
+
+var Article = connection.define('article',{
+    title: Sequelize.STRING,
+    body: Sequelize.TEXT
+});
