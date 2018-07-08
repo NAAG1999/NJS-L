429be7a4ede4017d23d2f070dde9c63609a11855
Nilesh <agnilesh8199@gmail.com>
2018-07-08 23:08:56 +0530

    Inserting first data in our database

--- sequelize-practise/app.js
+++ sequelize-practise/app.js
@@ -1,7 +1,32 @@
-var Sequelize = require('sequelize')
-var connection = new Sequelize('demo_schema', 'root', 'password'); // demo_schmea --> name of database, username, password
+var Sequelize = require('sequelize');
+//var connection = new Sequelize('demo_schema', 'root', 'password'); // demo_schmea --> name of database, username, password
+
+var connection = new Sequelize('demo_schema', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
 
 var Article = connection.define('article',{
     title: Sequelize.STRING,
     body: Sequelize.TEXT
 });
+
+connection.sync().then(function(){
+  Article.create({
+    title: 'demo title',  
+    body: 'Lorem ipsum fdsadsd'
+  });
+});
+
+// connection.sync().then(function(){
+//   Article.create({
+//     title: 'demo title',  
+//     body: 'asdad Lorem ipsum fdsadsd'
+//   });
+// });
+
+// connection.sync().then(function(){
+//   Article.findAll().then(function(articles){
+//     console.log(articles.length);
+//   });
+// });
