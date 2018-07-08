48b11f06a5bc97747a78d9bd0b61a212b2b6efe1
Nilesh <agnilesh8199@gmail.com>
2018-07-09 00:22:43 +0530

    adding dialects explicitly

--- sequelize-practise/hooks.js
+++ sequelize-practise/hooks.js
@@ -1,6 +1,7 @@
 var Sequelize = require('sequelize');
 
-var connection = new Sequelize('demo_schema', 'root', '');
+//var connection = new Sequelize('demo_schema', 'root', ''); This method is deprecated
+
 
 var Article = connection.define('article',{
     slug:{
@@ -10,7 +11,21 @@ var Article = connection.define('article',{
     title: Sequelize.STRING,
     body: Sequelize.TEXT
 },{
+    hooks:{
+        beforeValidate: function(){
+            console.log("Before Validation");
+        },
+        afterValidate: function(){
+            console.log("After Validation");
+        },
+        beforeCreate: function(){  
+            console.log("Before Creation");
+        },
+        afterCreate: function(){
+            console.log("After Creation");
+        }
 
+    }
 });
 
 connection
