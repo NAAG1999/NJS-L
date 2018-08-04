802aa58aa9d162c7cf712a731d523b5f896159a9
Nilesh <agnilesh8199@gmail.com>
2018-08-05 04:02:10 +0530

    Using arrays using process.argv in node.js

--- Note-App/practice/app.js
+++ Note-App/practice/app.js
@@ -7,4 +7,14 @@ const notes = require('./notes1.js');
 // var res = notes.addNums(7,-9);
 // console.log(res);
 
-console.log('Result: ', notes.addNums(7,-2));
+//console.log('Result: ', notes.addNums(7,-2));
+var command = process.argv[2];
+console.log('Command: ' + command);
+
+if(command === 'add'){
+    console.log('Adding new node');
+} else if (command === 'list'){
+    console.log('Listing all nodes');
+} else{
+    console.log('Command not recgonized');
+}
