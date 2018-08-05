0b87d891f1bcd5b93ad48a2011679708a7b00cf5
Nilesh <agnilesh8199@gmail.com>
2018-08-05 15:41:15 +0530

    Adding some style to the output on local server

--- Web-Server/playground1.js
+++ Web-Server/playground1.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', function(req,res) {
-    res.send('Hello Express!');
+    res.send('<h1>Hello Express!</h1>');
 });
 
 // Writing it in the es6 format we have:- 
