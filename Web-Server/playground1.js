eadd57a543db2e69683e1335fcc3bc4adb49682f
Nilesh <agnilesh8199@gmail.com>
2018-08-05 16:23:03 +0530

    Using routes on node.js

--- Web-Server/playground1.js
+++ Web-Server/playground1.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 var app = express();
 
-app.get('/', function(req,res) {
-    res.send('<h1>Hello Express!</h1>');
-});
+// app.get('/', function(req,res) {
+//     res.send('<h1>Hello Express!</h1>');
+// });
 
 // Writing it in the es6 format we have:- 
 
@@ -12,4 +12,21 @@ app.get('/', function(req,res) {
 //     res.send('Hello express!');
 // });
 
+app.get('/', (req, res) => {
+    res.send({
+        name: 'Nilesh',
+        lname : 'Aggarwal',
+        age : 20,
+    })
+})
+
+app.get('/about', (req,res) =>{
+    res.send('Welcome to the about of this web page');
+})
+
+app.get('/bad', (req,res) =>{
+    res.send({
+        errorMessage : 'Something bad just happened. Fault is at our end'
+    })
+})
 app.listen(3000);
